23e214ff398bad5555b3439b43716a4536de0042
wamweki boniface <94840519+bonnieace@users.noreply.github.com>
2024-06-13 13:30:50 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,38 @@
 import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
-import { LogIn } from 'lucide-react';
+import {LogIn} from 'lucide-react'
 import FileUpload from "@/components/FileUpload";
+export default async function Home(){
+  const{userId}=await auth()
+  const  isAuth=!!userId
+  return  <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="flex flex-col items-center text-center">
+        <div className="flex items-center">
+          <h1 className="mr-3 text-5xl font-semibold">          Chat with any PDF
+        </h1>
+        <UserButton afterSignOutUrl="/"/>
 
-export default async function Home() {
-  const { userId } = await auth();
-  const isAuth = !!userId;
-
-  return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col items-center text-center">
-          <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
-            <UserButton afterSignOutUrl="/" />
-          </div>
-          <div className="flex mt-2">
-            {isAuth &&
-              <Button>
-                Go to chats
-              </Button>
-            }
-          </div>
-          <p className="max-w-xl mt-1 text-lg">&quot;Welcome to ChatPDF: Where AI Enhances Your Document Conversations!&quot;</p>
-          <div className="w-full mt-4">
-            {isAuth ? (
-              <FileUpload />
-            ) : (
-              <Link href='/sign-in'>
-                <Button>&quot;Login to get Started&quot;
-                  <LogIn className="w-4 h-4 ml-2" />
-                </Button>
-              </Link>
-            )}
-          </div>
+        </div>
+        <div className="flex mt-2">
+          {isAuth&&
+          <Button>
+            Go to chats
+          </Button>}
+        </div>
+        <p className="max-w-xl mt-1 text-lg">"Welcome to ChatPDF: Where AI Enhances Your Document Conversations!"</p>
+        <div className="w-full mt-4">
+          {isAuth ?(<FileUpload/>):(
+            <Link href='/sign-in'>
+            <Button>Login to get Started
+            <LogIn className="w-4 h-4 ml-2"/>
+            </Button>
+            </Link>
+          ) }
         </div>
       </div>
     </div>
-  );
+  
+  </div>
 }
